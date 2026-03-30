926924e5d08f278096a87205c0d0a876f4d2b578
Pranav <pranavtilekar9999@gmail.com>
2026-03-30 09:21:00 +0530

    add root route

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/order", require("./routes/order"));
